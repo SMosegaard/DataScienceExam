8b394f007090160a72049197d7d5a3b2bb82bdb2
Soffedoffe <sofie.mosegaard@live.dk>
2025-05-23 11:03:28 +0200

    added cd ..

--- src/lagllama_setup.sh
+++ src/lagllama_setup.sh
@@ -5,3 +5,6 @@ cd lag-llama
 # Install requirements and the model from HuggingFace 
 pip install -r requirements.txt
 huggingface-cli download time-series-foundation-models/Lag-Llama lag-llama.ckpt --local-dir lag-llama
+
+# Navigate out of the Lag-Llama repo again
+cd ..
