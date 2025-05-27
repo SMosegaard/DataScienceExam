b16690b09eaa4f9124a487ac7b134cbc5b4ead89
SMosegaard <sofie.mosegaard@live.dk>
2025-05-27 16:01:41 +0200

    added the execusion of read_data to the setup bash

--- setup.sh
+++ setup.sh
@@ -5,8 +5,11 @@ python -m venv virt_env
 source ./virt_env/bin/activate
 
 # Install requirements
-pip install --upgrade pip
-pip install -r requirements.txt
+#pip install --upgrade pip
+#pip install -r requirements.txt
+
+# Download data from Hugging Face
+python src/read_data.py "$@"
 
 # Inform user
-echo "Successfully installed requirements.txt"
+echo "Successfully installed requirements and downdloaded the data from Hugging Face"
